7a9753297f133c03628ceafdc6bca4a8cca7ebaa
Javier Aviles <avileslopez.javier@gmail.com>
2017-12-03 01:19:14 +0100

    Angular-material installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
@@ -9,6 +11,8 @@ import { environment } from '../environments/environment';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, // imports components to be used from angular-material
     AngularFireModule.initializeApp(environment.firebase, 'triathlon-club'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
