aabc4627581ff398a853319f5696b2ffb4935457
Javier Aviles <avileslopez.javier@gmail.com>
2017-12-05 17:03:28 +0100

    Basic Events view, Event entity, Event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core/core.module';
 import { FormsModule } from '@angular/forms';
+import { EventsComponent } from './events/events/events.component';
+import { EventService } from './events/event.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule, // imports components to be used from angular-material
+    MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule, MatListModule, // imports components to be used from angular-material
     AngularFireModule.initializeApp(environment.firebase, 'triathlon-club'), // imports firebase/app needed for everything
     CoreModule,
     FormsModule
   ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ AppComponent, EventsComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [ EventService ]
 })
 export class AppModule {}
---
+++ src/app/events/events/events.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Event } from '../event';
+import { EventService } from '../event.service';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+
+  events: Event[];
+
+  constructor(private eventService: EventService) { }
+
+  ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents(): void {
+    this.eventService.getEvents()
+        .subscribe(events => this.events = events);
+  }
+
+}
