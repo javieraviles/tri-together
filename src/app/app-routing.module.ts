9a40b510b7c10bb8cb7017ea98bf75512f4eb5fc
Javier Aviles <avileslopez.javier@gmail.com>
2017-12-07 01:15:56 +0100

    Refactoring scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
-import { UserProfileComponent } from './users/user-profile/user-profile.component';
-import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './ui/user-profile/user-profile.component';
+import { UserLoginComponent } from './ui/user-login/user-login.component';
 
 import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: UserLoginComponent },
   { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: 'events', component: EventsListComponent, canActivate: [AuthGuard] }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule} from '@angular/material';
 
 import { AppRoutingModule } from './/app-routing.module';
 import { environment } from '../environments/environment';
@@ -15,23 +15,23 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { EventModule } from './events/shared/event.module';
-import { UserProfileComponent } from './users/user-profile/user-profile.component';
-import { LoginComponent } from './login/login.component';
+import { UiModule } from './ui/shared/ui.module';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatInputModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule,
+    MatButtonModule, MatToolbarModule,
     AngularFireModule.initializeApp(firebaseConfig, 'triathlon-club'),
     CoreModule,
     AppRoutingModule,
     SharedModule,
     EventModule,
-    FormsModule
+    FormsModule,
+    UiModule
   ],
-  declarations: [ AppComponent, UserProfileComponent, LoginComponent ],
+  declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
